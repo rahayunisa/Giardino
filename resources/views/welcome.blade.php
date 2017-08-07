e9107a0900931b18cbf4105b9292a5167f3cb3f8
User <User>
2017-08-07 17:26:44 +0700

    Project Aplikasi Jual Beli

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"> Halo </div> 
+                <div class="panel-body">
+                    <center><h4>Selamat Datang di Aplikasi Jual Beli Sepatu dan Sandal</h4></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
